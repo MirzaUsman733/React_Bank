b0aa896f0c4144fdcd97bb46071fde5cf30505d6
mirzausman <mirzausman9006@gmail.com>
2024-03-24 12:31:12 +0500

    react bank with firebase

---
+++ src/Config/Firebase.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth,GoogleAuthProvider, FacebookAuthProvider,  GithubAuthProvider  } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAW6icZ3V1WSwUrbPliU35VlftKlH6vxo",
+//   authDomain: "jajja-bank-app.firebaseapp.com",
+//   projectId: "jajja-bank-app",
+//   storageBucket: "jajja-bank-app.appspot.com",
+//   messagingSenderId: "165227390733",
+//   appId: "1:165227390733:web:ed1898ba92d34b85ac3244",
+//   measurementId: "G-EHHKQ561B6"
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDrspRxqkxvCl98XcUts1tYom2FI7Zyyu8",
+  authDomain: "mirza-bank.firebaseapp.com",
+  projectId: "mirza-bank",
+  storageBucket: "mirza-bank.appspot.com",
+  messagingSenderId: "305762007545",
+  appId: "1:305762007545:web:76fdfb8fba53fb5b422528",
+  measurementId: "G-7PG0VS1N14"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const facebookProvider = new FacebookAuthProvider();
+const githubProvider = new GithubAuthProvider();
+export {auth, analytics,firestore,provider,facebookProvider,githubProvider};
